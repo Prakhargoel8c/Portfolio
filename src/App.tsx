288fb2bfd350c8bb6c2f3a8b4bd4ecf0f86a5c9e
Prakhar Goel <rockingprakhar.987@outlook.com>
2024-01-19 10:42:16 +0530

    Crated Basic Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,15 @@
-const App = () => <div className="w-dvw h-dvh bg-white"></div>;
+import { BrowserRouter } from 'react-router-dom';
+import { Hero, Navbar } from './components';
+
+const App = () => (
+  <BrowserRouter>
+    <div className="z-0 relative bg-primary">
+      <div className="bg-center bg-hero-pattern bg-no-repeat bg-cover">
+        <Navbar />
+        <Hero />
+      </div>
+    </div>
+  </BrowserRouter>
+);
 
 export default App;
