aeeea7885fe109ca9e30dd9a6962f2ffc436d3a5
Prakhar Goel <rockingprakhar.987@outlook.com>
2024-01-20 18:46:09 +0530

    Added Mobile Toggle

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,11 +2,12 @@ import { useState } from 'react';
 import { cn } from '../utils/tailwind';
 import { styles } from '../styles';
 import { Link } from 'react-router-dom';
-import { logo } from '../assets';
+import { close, logo, menu } from '../assets';
 import { navLinks } from '../constants';
 
 const Navbar = () => {
   const [active, setActive] = useState('');
+  const [toggle, setToggle] = useState(false);
   return (
     <nav className={cn('w-full flex items-center fixed z-20 top-0 py-5 bg-primary', styles.paddingX)}>
       <div className="w-full flex max-w-7xl justify-between items-center mx-auto">
@@ -35,6 +36,28 @@ const Navbar = () => {
             </li>
           ))}
         </ul>
+        <div className="sm:hidden flex flex-1 justify-end items-center">
+          <img src={toggle ? close : menu} alt="menu" className="w-7 h-7 object-contain" onClick={() => setToggle(!toggle)} role="button" />
+          <div className={cn(toggle ? 'flex' : 'hidden', 'p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-36 z-10 rounded-xl')}>
+            <ul className="flex flex-col list-none justify-end items-start gap-4">
+              {navLinks.map((link) => (
+                <li
+                  key={link.id}
+                  className={cn('font-poppins text-base font-medium cursor-pointer', {
+                    'text-white': active === link.title,
+                    'text-secondary': active !== link.title,
+                  })}
+                  onClick={() => {
+                    setActive(link.title);
+                    setToggle(false);
+                  }}
+                >
+                  <a href={`#${link.id}`}>{link.title}</a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
       </div>
     </nav>
   );
