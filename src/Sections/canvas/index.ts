2ce607719508c200801bd2a405a32062b622a0dd
Prakhar Goel <rockingprakhar.987@outlook.com>
2024-03-01 14:07:30 +0530

    Cleaned Repository

--- src/Sections/canvas/index.ts
+++
@@ -1,6 +0,0 @@
-import BallCanvas from "./Ball";
-import ComputersCanvas from "./ComputerCanvas";
-import EarthCanvas from "./Earth";
-import StarsCanvas from "./Stars";
-
-export { BallCanvas, ComputersCanvas, EarthCanvas, StarsCanvas };
--- src/components/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "./MatrixBackground";
-export * from "./button.tsx";
-export * from "./typography.tsx";
