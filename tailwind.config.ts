5f9f5e1223560fc82b5cdaead443a5ae22f96438
Prakhar Goel <rockingprakhar.987@outlook.com>
2024-03-01 15:07:23 +0530

    Added Pakages

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,8 @@
 import { type Config } from "tailwindcss";
 import { fontFamily } from "tailwindcss/defaultTheme";
 
+const { default: flattenColorPalette } = require("tailwindcss/lib/util/flattenColorPalette");
+
 export default {
   content: ["./src/**/*.tsx"],
   theme: {
@@ -10,5 +12,15 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [addVariablesForColors],
 } satisfies Config;
+
+// This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
+function addVariablesForColors({ addBase, theme }: any) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(Object.entries(allColors).map(([key, val]) => [`--${key}`, val]));
+
+  addBase({
+    ":root": newVars,
+  });
+}
