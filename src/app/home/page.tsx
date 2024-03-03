01ecc5ef566fe11c6648a7b15a75928c3b797a54
Prakhar Goel <rockingprakhar.987@outlook.com>
2024-03-03 10:34:49 +0530

    Added grid background

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -2,9 +2,11 @@ import { Spotlight } from "~/components/ui/Spotlight";
 
 export default function HomePage() {
   return (
-    <div className="h-dvh w-full rounded-md flex md:items-center md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden">
+    <div className="h-dvh w-full dark:bg-black bg-white  dark:bg-grid-white/[0.1] bg-grid-black/[0.2] relative flex items-center justify-center">
+      <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+
       <Spotlight className="-top-40 left-0 md:left-60 md:-top-20" fill="white" />
-      <div className=" p-4 max-w-7xl  mx-auto relative z-10  w-full pt-20 md:pt-0">
+      <div className="p-4 max-w-7xl  mx-auto relative z-10  w-full pt-20 md:pt-0">
         <h1 className="text-4xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
           Web Development <br /> is the new trend.
         </h1>
